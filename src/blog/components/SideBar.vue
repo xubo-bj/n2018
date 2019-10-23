d886e38a978d45bb8904d08ae719a3c35bfcc4a8
xubo-bj <xubo_bj@qq.com>
2019-10-23 22:50:03 +0800

    '1023-2'

--- src/blog/components/SideBar.vue
+++ src/blog/components/SideBar.vue
@@ -1,37 +1,21 @@
 <template>
-	<div>
-		<router-link-wrapper
-			to="/blog/dashboard"
-			link-name="概述"
-			@select_tab="select_tab"
-		/>
-		<router-link-wrapper to="/blog/post/list" link-name="文章管理" />
-		<transition>
-			<div>
-				<router-link-wrapper
-					to="/blog/post/list"
-					link-name="文章列表"
-				/>
-				<router-link-wrapper
-					to="/blog/post/create"
-					link-name="添加文章"
-				/>
-			</div>
-		</transition>
-		<router-link-wrapper to="/blog/page/list" link-name="页面管理" />
-		<transition>
-			<div>
-				<router-link-wrapper
-					to="/blog/page/list"
-					link-name="页面列表"
-				/>
-				<router-link-wrapper
-					to="/blog/page/create"
-					link-name="添加页面"
-				/>
-			</div>
-		</transition>
-	</div>
+  <div>
+    <router-link-wrapper to="/blog/dashboard" link-name="概述" @select_tab="select_tab" />
+    <router-link-wrapper to="/blog/post/list" link-name="文章管理" @select_tab="select_tab" />
+    <transition>
+      <div class="trans-elem" v-if="currentTab[0]==='post'">
+        <router-link-wrapper to="/blog/post/list" link-name="文章列表" @select_tab="select_tab" />
+        <router-link-wrapper to="/blog/post/create" link-name="添加文章" @select_tab="select_tab" />
+      </div>
+    </transition>
+    <router-link-wrapper to="/blog/page/list" link-name="页面管理" @select_tab="select_tab" />
+    <transition>
+      <div class="trans-elem" v-if="currentTab[0]==='page'">
+        <router-link-wrapper to="/blog/page/list" link-name="页面列表" @select_tab="select_tab" />
+        <router-link-wrapper to="/blog/page/create" link-name="添加页面" @select_tab="select_tab" />
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script lang="ts">
@@ -41,26 +25,40 @@ import { SELECT_TAB } from "../store/mutation-types.ts";
 import Component from "vue-class-component";
 import VueRouter from "vue-router";
 import RouterLinkWrapper from "./RouterLinkWrapper";
+import { stateShape } from "../store/state";
 
 @Component({
-	computed: mapState(["currentTab"]),
-	methods: mapActions([SELECT_TAB]),
-	components: { RouterLinkWrapper }
+  computed: mapState(["currentTab"]),
+  methods: mapActions([SELECT_TAB]),
+  components: { RouterLinkWrapper }
 })
 export default class SideBar extends Vue {
-	SELECT_TAB!: (obj: object) => void;
-	select_tab(obj: any) {
-		console.log("obj  :", obj);
-		this.SELECT_TAB(obj);
-	}
+  SELECT_TAB!: (obj: object) => void;
+  currentTab!: (state: stateShape) => string[];
+  select_tab(obj: any) {
+    console.log("obj  :", obj);
+    this.SELECT_TAB(obj);
+  }
+  updated() {
+    console.log("updated", this.currentTab);
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.router-link {
-	display: block;
-	// color: rgba(255, 255, 255, 0.5);
-	height: 50px;
-	border: 1px solid #000;
+.v-enter-active,
+.v-leave-active {
+  transition: height 1s;
 }
+.v-enter,
+.v-leave-to {
+  height: 0px;
+}
+.v-leave,
+.v-enter-to {
+  height: 100px;
+}
+// .trans-elem {
+//   height: 100px;
+// }
 </style>
