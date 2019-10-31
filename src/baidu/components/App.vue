419bb76ca631f95c83b0ff77bb50106afe8cf985
xubo-bj <xubo_bj@qq.com>
2019-10-31 11:42:22 +0800

    1031-1

--- src/baidu/components/App.vue
+++ src/baidu/components/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="container">
-    <top-bar />
-  </div>
+	<div class="container">
+		<top-bar />
+		<logo />
+	</div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import TopBar from "./TopBar";
+import Logo from "./Logo";
 import Component from "vue-class-component";
 @Component({
-  components: { TopBar }
+	components: { TopBar, Logo }
 })
 export default class App extends Vue {}
 </script>
 
-<style lang="scss" scoped>
-.container {
-  border: 5px solid #000;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/baidu/components/TopBar.vue
+++ src/baidu/components/TopBar.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>123</div>
+	<div class="top-bar">
+		<a class="user btn"><span class="user-icon"></span></a>
+		<a class="menu btn" href="https://m.baidu.com/usrprofile?action=square"
+			><span class="menu-icon"></span
+		></a>
+	</div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({});
 </script>
+
+<style lang="scss" scoped>
+.top-bar {
+	display: flex;
+	justify-content: space-between;
+	height: 52px;
+	margin-bottom: 9px;
+}
+.btn {
+	flex-basis: 44px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	.user-icon {
+		width: 20px;
+		height: 20px;
+		background-image: url(/baidu/images/btn_icon_1.png);
+		background-size: contain;
+	}
+	.menu-icon {
+		width: 16px;
+		height: 16px;
+		background-image: url(/baidu/images/btn_icon_2.png);
+		background-size: contain;
+	}
+}
+</style>
