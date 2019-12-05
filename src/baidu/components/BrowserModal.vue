9e7cc4c36e61ceaef5d63dfbca619dff61accb4c
xubo-bj <xubo_bj@qq.com>
2019-12-05 17:48:10 +0800

    1205

--- src/baidu/components/BrowserModal.vue
+++ src/baidu/components/BrowserModal.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="browser-modal" v-if="browserModal">
 		<div class="modal-box">
-			<div class="modal-head"></div>
+			<div class="modal-head">
+				<div class="btn" @click="HIDE_BROWSER_MODAL">×</div>
+			</div>
 			<div class="modal-body">请使用移动浏览器，获取更佳的浏览效果</div>
 			<div class="modal-foot"></div>
 		</div>
@@ -30,33 +32,49 @@ export default class BrowserModal extends Vue {}
 	z-index: 20;
 	display: flex;
 	justify-content: center;
-	.modal-box {
-		align-self: start;
+}
+
+.modal-box {
+	align-self: start;
+	position: relative;
+	z-index: 1;
+	margin-top: 150px;
+	width: 400px;
+	background-color: #fff;
+	.modal-head {
 		position: relative;
-		z-index: 1;
-		margin-top: 150px;
-		width: 400px;
-		background-color: #fff;
-		.modal-head {
-			height: 70px;
-			border-bottom: 1px solid #dee2e6;
-		}
-		.modal-body {
-			height: 70px;
-			border-bottom: 1px solid #dee2e6;
-		}
-		.modal-foot {
-			height: 70px;
+		height: 70px;
+		border-bottom: 1px solid #dee2e6;
+		.btn {
+			position: absolute;
+			right: 0;
+			padding: 16px;
+			// font-size: 1.5rem;
+			font-size: 30px;
+			font-weight: 700;
+			line-height: 1;
+			color: #000;
+			opacity: 0.5;
+			text-shadow: 0 1px 0 #fff;
 		}
 	}
-	.modal-mask {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background-color: #000;
-		opacity: 0.5;
+	.modal-body {
+		padding: 20px 0;
+		border-bottom: 1px solid #dee2e6;
+		text-align: center;
 	}
+	.modal-foot {
+		height: 70px;
+	}
+}
+
+.modal-mask {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: #000;
+	opacity: 0.5;
 }
 </style>
