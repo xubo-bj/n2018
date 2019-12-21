13f7196ebca7ea53e32533936a961a8d77862cd7
xubo-bj <xubo_bj@qq.com>
2019-12-21 22:22:25 +0800

    191221

--- config/homepage.gulpfile.js
+++ config/homepage.gulpfile.js
@@ -6,90 +6,90 @@ sass.compiler = require("node-sass");
 const path = require("path");
 const browserSync = require("browser-sync").create();
 const appPort = require("../config").appPort;
-const srcPath = "./src/ctrip";
-const destPath = "./public/ctrip";
+const srcPath = "./src/homepage";
+const destPath = "./public/homepage";
 
 function sassToCss() {
-    return src(`${srcPath}/sass/**/*.scss`)
-        .pipe(sass().on("error", sass.logError))
-        .pipe(dest(path.join(destPath, "css")));
+	return src(`${srcPath}/sass/*.scss`)
+		.pipe(sass().on("error", sass.logError))
+		.pipe(dest(path.join(destPath, "css")));
 }
 
 function watch_sass(cb) {
-    watch(
-        `${srcPath}/sass/**/*.scss`,
-        {
-            events: ["change"]
-        },
-        sassToCss
-    );
-    cb();
+	watch(
+		`${srcPath}/sass/*.scss`,
+		{
+			events: ["change"]
+		},
+		sassToCss
+	);
+	cb();
 }
 
 function refresh(cb1) {
-    browserSync.init({
-        proxy: `127.0.0.1:${appPort}`
-    });
-    watch(
-        ["./views/ctrip/index.ejs", `${destPath}/**`],
-        {
-            events: ["change"]
-        },
-        function(cb2) {
-            browserSync.reload();
-            cb2();
-        }
-    );
-    cb1();
+	browserSync.init({
+		proxy: `127.0.0.1:${appPort}`
+	});
+	watch(
+		["./views/*.ejs", `${destPath}/**`],
+		{
+			events: ["change"]
+		},
+		function(cb2) {
+			browserSync.reload();
+			cb2();
+		}
+	);
+	cb1();
 }
 
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
-var concat = require("gulp-concat");
+// var sourcemaps = require("gulp-sourcemaps");
+// var babel = require("gulp-babel");
+// var concat = require("gulp-concat");
 
 /**
  * babel中是 @babel/preset-env   而babel-gulp中是 @babel/env
  */
 
-function es6ToEs5() {
-    console.log("es6 to es5");
-    return (
-        src(`${srcPath}/es6/*.js`)
-            .pipe(sourcemaps.init())
-            .pipe(
-                babel({
-                    presets: ["@babel/preset-env"]
-                })
-            )
-            // .pipe(concat("all.js"))
-            .pipe(sourcemaps.write("."))
-            .pipe(dest(`${destPath}/es5`))
-    );
-}
+// function es6ToEs5() {
+// 	console.log("es6 to es5");
+// 	return (
+// 		src(`${srcPath}/es6/*.js`)
+// 			.pipe(sourcemaps.init())
+// 			.pipe(
+// 				babel({
+// 					presets: ["@babel/preset-env"]
+// 				})
+// 			)
+// 			// .pipe(concat("all.js"))
+// 			.pipe(sourcemaps.write("."))
+// 			.pipe(dest(`${destPath}/es5`))
+// 	);
+// }
 
-function watchEs6ToEs5(cb) {
-    watch(
-        `${srcPath}/es6/**`,
-        {
-            events: ["change"]
-        },
-        es6ToEs5
-    );
-    cb();
-}
+// function watchEs6ToEs5(cb) {
+// 	watch(
+// 		`${srcPath}/es6/**`,
+// 		{
+// 			events: ["change"]
+// 		},
+// 		es6ToEs5
+// 	);
+// 	cb();
+// }
 
 const del = require("delete");
 
 function delOldCss(cb) {
-    // Use the `delete` module directly, instead of using gulp-rimraf
-    del([`${destPath}/css/**`], cb);
+	// Use the `delete` module directly, instead of using gulp-rimraf
+	del([`${destPath}/stylesheets/**`], cb);
 }
-function delOldEs5(cb) {
-    del([`${destPath}/es5/**`], cb);
-}
+// function delOldEs5(cb) {
+// 	del([`${destPath}/es5/**`], cb);
+// }
 
 exports.default = parallel(
-    series(delOldCss, sassToCss, watch_sass),
-    series(delOldEs5, es6ToEs5, watchEs6ToEs5),
-    refresh
+	series(delOldCss, sassToCss, watch_sass),
+	// series(delOldEs5, es6ToEs5, watchEs6ToEs5),
+	refresh
 );
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-'use strict';
+"use strict";
 
-exports.ctrip = require('./config/ctrip.gulpfile').default
-exports.note= require('./config/note.gulpfile').default
-exports.baidu= require('./config/baidu.gulpfile').default
-exports.blog= require('./config/blog.gulpfile').default
+exports.ctrip = require("./config/ctrip.gulpfile").default;
+exports.note = require("./config/note.gulpfile").default;
+exports.baidu = require("./config/baidu.gulpfile").default;
+exports.blog = require("./config/blog.gulpfile").default;
+exports.default = require("./config/homepage.gulpfile").default;
